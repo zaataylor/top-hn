80b8a8ba4769d2c62fcdff0cca00542f275fa7fc
Zachary Taylor <zataylor@ncsu.edu>
2019-08-20 11:27:25 -0400

    bug fixes

--- tophn.py
+++ tophn.py
@@ -27,8 +27,11 @@ except:
 #current top story id on HN
 top_hn_id = None
 
-#timeout interval of 2 minutes to test
-INTERVAL = 60*2
+#set of top story ids used later on
+id_set = set()
+
+#timeout interval of 10 minutes to test
+INTERVAL = 60*10
 
 #HN Firebase API URL 
 HN_API_BASE = "https://hacker-news.firebaseio.com/v0/"
@@ -36,7 +39,7 @@ HN_API_BASE = "https://hacker-news.firebaseio.com/v0/"
 #HN Website Base URL
 HN_BASE = "https://news.ycombinator.com/"
 
-#loop that will run every 2 minutes
+#loop that will run every 10 minutes
 while True:
     response = requests.get(HN_API_BASE + "topstories.json")
     #first time running
@@ -45,6 +48,9 @@ while True:
 
         #first story in list of 500 top stories
         top_hn_id = int(top_hn_ids[0])
+        
+        #add story id to set, disqualifying it from appearing again
+        id_set.add(top_hn_id)
 
         #actual JSON content of story associated with ID
         response = requests.get(HN_API_BASE + "/item/{}.json".format(top_hn_id))
@@ -52,8 +58,11 @@ while True:
         
         #tweet about top story
         status = "Poster: {}\nTitle: {}\nURL: {}".format(top_story['by'], top_story['title'], HN_BASE + "item?id=" + str(top_story['id']))
-        api.update_status(status)
-        print("Top HN ID: {} Posted: {}".format(str(top_hn_id), status, ))
+        try:
+            api.update_status(status)
+            print("Top HN ID: {} Post: {}".format(str(top_hn_id), status))
+        except tweepy.TweepError:
+            print("Tweet was a duplicate and was not posted")
     else:
         current_hn_ids = response.text.replace('[', '').replace(']', '').split(sep=',')
 
@@ -61,13 +70,19 @@ while True:
         current_hn_id = int(current_hn_ids[0])
 
         #tweet with updated top post
-        if(current_hn_id != top_hn_id):
+        if(current_hn_id not in id_set):
+
+            id_set.add(current_hn_id)
             response = requests.get(HN_API_BASE + "/item/{}.json".format(current_hn_id))
             current_story = json.loads(response.text)
+
             #tweet here
             status = "New top story!\nPoster: {}\nTitle: {}\nURL: {}".format(current_story['by'], current_story['title'], HN_BASE + "item?id=" + str(current_story['id']))
-            api.update_status(status)
-            print("Top HN ID: {} Current HN ID: {} Posted: {}".format(str(top_hn_id), str(current_hn_id), status))
+            try:
+                api.update_status(status)
+                print("Top HN ID: {} Current HN ID: {} Post: {}".format(str(top_hn_id), str(current_hn_id), status))
+            except tweepy.TweepError:
+                print("Tweet was a duplicate and was not posted")
         else:
-            print("Previous top story is still ranked #1!Top HN ID: {} Current HN ID: {}".format(str(top_hn_id), str(current_hn_id)))
+            print("Previous top story is still ranked #1! Top HN ID: {} Current HN ID: {}".format(str(top_hn_id), str(current_hn_id)))
     time.sleep(INTERVAL)
